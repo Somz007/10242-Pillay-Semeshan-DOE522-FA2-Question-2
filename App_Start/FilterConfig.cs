4a8dbdcf78647019a601ae33693e2b0f31879d76
semeshan <psemeshan@gmail.com>
2022-08-26 10:59:14 +0200

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _10242_Pillay_Semeshan_DOE522_FA2_Question_2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_10242_Pillay_Semeshan_DOE522_FA2_Question_2.MvcApplication" Language="C#" %>
